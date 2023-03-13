d1c918ce5f461e71d0ba0dc3d1c38bca07244d72
Ruperth <ruperthnyagesoa@gmail.com>
2023-03-13 11:56:53 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+   if(distance <= 400) {return "This one is on me!"}
+   else if(distance>= 2000 && distance <= 2499){return "I will gladly take your thirty bucks."}
+   else if(distance > 2500){return"No can do."} 
+}
+function ternaryCheckCity(city){
+  // Write your code here!
+  return("NYC"=== city ? "Ok, sounds good.": "No go.")
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+  switch(tip) {
+       case "generous":return  "Thank you so much.";
+  break;
+       case "not as generous": return "Thank you.";
+  break;
+        default:return "Bye."
+}}
